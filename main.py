18b6a858dedc93a24e0ee46f17be60ba2916e91c
FreakHuhn <mcbabamuck@googlemail.com>
2025-12-29 16:05:50 +0100

    another guided project by bootdev

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+import argparse
+from google.genai import types
+
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    
+    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_input", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_input)])]
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="models/gemini-2.5-flash", 
+        contents= messages
+    )
+    
+    if response.usage_metadata is None:
+        raise RuntimeError("Usage metadata is None - API request may have failed")
+    
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
